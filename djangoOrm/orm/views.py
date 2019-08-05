a6f1100f76e902c01840c388049748a0ba10f1e2
kel <akyl_0221@mail.ru>
2019-08-05 15:05:35 +0600

    sub_query create

--- djangoOrm/orm/views.py
+++ djangoOrm/orm/views.py
@@ -7,11 +7,12 @@ from .models import TaskStatuses, Task
 def index(request):
     start_date = date(2019, 4, 2)
     end_date = date(2019, 9, 2)
+    sub_queryset = TaskStatuses.objects.annotate(max_datetime=models.Max('created'))\
+        .filter(task_id=models.OuterRef('id'), status_type=3)\
+        .values('max_datetime')
     queryset = Task.objects.filter(created__range=(start_date, end_date))\
         .annotate(Checked=models.Subquery(
-            TaskStatuses.objects.annotate(max_datetime=models.Max('created'))
-            .filter(task_id=models.OuterRef('id'), status_type=3)
-            .values('max_datetime'), output_field=models.DateTimeField()))\
+            sub_queryset, output_field=models.DateTimeField()))\
         .values('created', 'description', 'Checked')
     print(queryset.query)
     return HttpResponse(queryset)
